a1bc27eb0af5bc9221710f5827ac6e9ef277c5d0
Benjamin Brandau <codingbrandau@gmail.com>
2020-11-22 19:19:44 -0700

    created tab bar in scene Delegate

---
+++ GitHubFollowers/GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Ben Brandau Brandau on 11/22/20.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+    }
+}
---
+++ GitHubFollowers/GitHubFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Ben Brandau Brandau on 11/22/20.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+
+}
+}
